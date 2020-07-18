4d0390e7ee4c37837787925109c900619d4df66c
wankhede04 <wankhedevijay04@gmail.com>
2020-07-18 16:59:32 +0530

    Added note-metadata component.

---
+++ src/app/note-metadata/note-metadata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-metadata',
+  templateUrl: './note-metadata.component.html',
+  styleUrls: ['./note-metadata.component.scss']
+})
+export class NoteMetadataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
