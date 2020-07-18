d39574a827b422e46156c6ca9226ac49ad8d6fb6
wankhede04 <wankhedevijay04@gmail.com>
2020-07-18 16:40:11 +0530

    Updated app module with NGXS configurations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsStoragePluginModule, StorageOption } from '@ngxs/storage-plugin';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { ngxsConfig } from './store/ngxs.config';
+import { environment } from '../environments/environment';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
@@ -15,6 +22,21 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+    NgxsModule.forRoot(
+      [],
+      ngxsConfig),
+    NgxsStoragePluginModule.forRoot({
+      storage: StorageOption.SessionStorage,
+      key: []
+    }),
+    NgxsRouterPluginModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      name: 'NOTES_APP',
+      disabled: environment.production
+    }),
+    NgxsLoggerPluginModule.forRoot({
+      disabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
