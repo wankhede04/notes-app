d864f2cff4ca0beda4482e72296bd2ca95cdff13
wankhede04 <wankhedevijay04@gmail.com>
2020-07-18 17:28:06 +0530

    Added note-board component.

---
+++ src/app/note-board/note-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-board',
+  templateUrl: './note-board.component.html',
+  styleUrls: ['./note-board.component.scss']
+})
+export class NoteBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
