2dbf19004a3d072bb1fac6dedd8f1026bb9638e7
wankhede04 <wankhedevijay04@gmail.com>
2020-07-19 16:39:11 +0530

    Added forms module for using ng model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
@@ -33,6 +34,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
     NgxsModule.forRoot(
       [NoteDetailsState],
