672d0d591ecc032f28c7211ed1fe9e1825f15504
wankhede04 <wankhedevijay04@gmail.com>
2020-07-19 16:42:21 +0530

    Added functionality to textarea for showing selected note description.

--- src/app/note-board/note-board.component.ts
+++ src/app/note-board/note-board.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, SimpleChange } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { isEmpty } from 'lodash';
+import { INotesDetails } from '../store/notes/notes.model';
 
 @Component({
   selector: 'app-note-board',
@@ -6,11 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-board.component.scss']
 })
 export class NoteBoardComponent implements OnInit {
+
+  @Output()
+  public noteDetailsEvent: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public allNotes: INotesDetails[];
+
+  @Input()
+  public currentNoteID: string;
+
   public currentDateTime: Date = new Date();
+  private _textareaValue: BehaviorSubject<string> = new BehaviorSubject('');
+
+  public get textareaValue(): string {
+    return this._textareaValue.getValue();
+  }
+
+  public set textareaValue(inputValue: string) {
+    this._textareaValue.next(inputValue);
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChange) {
+    let currentNote = this.allNotes.filter(note => note.noteID === this.currentNoteID);
+    this.textareaValue = isEmpty(currentNote) ? '' : currentNote[0].noteDescription;
+  }
+
+  public noteValue() {
+    this.noteDetailsEvent.emit(this.textareaValue)
   }
 
 }
